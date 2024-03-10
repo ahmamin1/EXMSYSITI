6c66c6d115f2786081955ee28bcbd7bb9f4abb1d
Ahmed Amin <ahmamin11@gmail.com>
2024-03-10 23:30:50 +0200

    seconed commit

---
+++ DbModels/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbModels
+{
+   
+    public class User: BaseEntity
+    {
+        [Key]
+        public int UserId { get; set; }
+       public string Username { get; set; }
+        public string Password { get; set; }
+      
+    }
+
+    public class Student : User
+    {
+        public ICollection<Subject> Subjects { get; set; }
+    }
+
+    public class Teacher : User
+    {
+
+        public ICollection<Subject> Subjects { get; set; }
+    }
+
+    public class Admin : User
+    {
+       
+    }
+
+}
